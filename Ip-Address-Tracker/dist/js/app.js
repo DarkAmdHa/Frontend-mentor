9a86d5f51c0d509d94a5b85c992622a52d6283c8
Hamid <hamidtahir2000@gmail.com>
2021-08-14 04:18:34 +0500

    Marker

--- Ip-Address-Tracker/dist/js/app.js
+++ Ip-Address-Tracker/dist/js/app.js
@@ -1,5 +1,10 @@
 let map = L.map('map').setView([51.505, -0.09], 13);
-let marker = L.marker([51.5,-0.09]).addTo(map);
+var blackIcon = L.icon({
+    iconUrl: './images/icon-location.svg',
+
+});
+let marker = L.marker([51.5,-0.09],{icon: blackIcon}).addTo(map);
+
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
 		maxZoom: 18,
 		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
